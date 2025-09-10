0d343facfcd54040b0cf61ab79ae67121a2d290b
allright <allright@example.com>
2025-09-10 16:55:05 +0900

    bootstrap test

--- src/routes/menuData.js
+++ src/routes/menuData.js
@@ -50,5 +50,12 @@ export const menuData = [
            
     ]
 },
+{
+    chapter : "ch06. IT Cafe",
+      items: [
+        {path : '/coffee_bread_01', label : 'IT Cafe 01'},
+        {path : '/coffee_bread_02', label : 'IT Cafe 02'},
+      ]
+}
 // 여기에 4장, 5장 ... 내용을 계속 추가하면 됩니다.
 ];
